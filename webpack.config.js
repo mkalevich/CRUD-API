1f723dcd71bf5218f203bc3ff00967a35e669803
Maksim Kalevich <maksim_kalevich@epam.com>
2024-02-07 22:14:43 +0300

    develop: Create package.json and configure webpack.

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  mode: "production",
+  target: "node",
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    filename: "build.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [new CleanWebpackPlugin()],
+};
