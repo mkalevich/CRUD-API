2b2c276dee548a7c571ab2df342e21737b98d1c0
Maksim Kalevich <maksim_kalevich@epam.com>
2024-02-11 18:43:31 +0300

    develop: Update db

--- src/db/users.ts
+++ src/db/users.ts
@@ -5,10 +5,4 @@ export const users = [
     id: "6F9619FF-8B86-D011-B42D-00CF4FC964FF",
     hobbies: ["nodejs, games"],
   },
-  {
-    username: "Egosya",
-    age: 4,
-    id: "6F9619FF-8B86-D011-B42D-00CF4FC964FA",
-    hobbies: ["cars, youtube"],
-  },
 ];
